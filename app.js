5986245b1be6938cad5fcb16e759d2c07f1d8814
indraarianggi <indraarianggi@gmail.com>
2018-02-22 10:34:38 +0700

    create & remove directory

--- app.js
+++ app.js
@@ -3,32 +3,34 @@
  */
 
 // import module
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function(name) {
-    this.name = name;
-};
+/**
+ Synchronous process
+ 
+// Create directory
+fs.mkdirSync('assets');
 
-// Make Person inherits event emitter
-util.inherits(Person, events.EventEmitter);
+// Delete directory
+fs.rmdirSync('assets');
 
-// Make object from Person
-var indra = new Person('indra');
-var arianggi = new Person('arianggi');
-var surya = new Person('surya');
-var people = [indra, arianggi, surya];
+*/
 
 
-// Bind the event with the action
-people.forEach(function(person) {
-    person.on('speak', function(msg) {
-        console.log(person.name + " said: " + msg);
+/**
+ Asynchronous process
+*/
+
+// Create directory
+fs.mkdir('assets', function() {
+    fs.readFile('readMe.txt', 'utf8', function(err, data) {
+        fs.writeFile('./assets/writeMe.txt', data + " [output of writing file]");
     });
 });
 
 
-// Fire the event
-indra.emit('speak', 'I learn Node.js now');
-arianggi.emit('speak', 'That awesome dude');
-surya.emit('speak', 'Can we learn it together?');
+// Delete a file there is in the folder first
+fs.unlink('./assets/writeMe.txt', function() {
+    // then delete the folder/directory
+    fs.rmdir('assets');
+});
