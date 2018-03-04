05ed21f19009cdbfc67e16527a3bfc609eb7c518
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2018-03-04 19:35:52 +0700

    basic routing

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const url = require('url');
 
-var server = http.createServer();
+var makeServer = (request, response) => {
+    let path = url.parse(request.url).pathname;
+    console.log(path);
 
-server.on('request', (request, response) => {
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello World');
+    // basic routing
+    if (path==='/') {
+        response.writeHead(200, {'Content-Type':'text/plain'});
+        response.write('Hello World!');
+    } else if (path==='/about') {
+        response.writeHead(200, {'Content-Type':'text/plain'});
+        response.write('About Page');
+    } else if (path==='/blog') {
+        response.writeHead(200, {'Content-Type':'text/plain'});
+        response.write('Blog Page');
+    } else {
+        response.writeHead(200, {'Content-Type':'text/plain'});
+        response.write('404, Page Not Found');
+    }
+
     response.end();
-});
+}
+
+var server = http.createServer(makeServer);
 
 server.listen(3000, () => {
     console.log('Node server created at port 3000');
