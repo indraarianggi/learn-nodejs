322b387533da9eab9549c891e205a6e0a9304a76
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2018-03-04 20:33:27 +0700

    RESTful API

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express');
+const users = require('./users');
 
 var server = express();
 
+// setting the port
 server.set('port', process.env.PORT || 3000);
 
 // basic routing
 server.get('/', (request, response) => {
-    response.send('Home Page');
+    response.sendFile(__dirname + '/index.html');
 });
 
-server.get('/about', (request, response) => {
-    response.send('About Page');
+server.get('/users', (request, response) => {
+    response.json(users);
 });
 
-
-// express error handling middleware
-server.use((request, response) => {
-    response.type('text/plain');
-    response.status(505);
-    response.send('Error Page');
-})
 
 // binding to a port
 server.listen(3000, () => {
